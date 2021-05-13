a992e05d4094bb1b244a539445013b2f7af32f97
Matt Lefebvre <matthewelefebvre@gmail.com>
2021-05-13 17:00:14 -0400

    IT WORKS

--- src/ServerReadFromClientThread.java
+++ src/ServerReadFromClientThread.java
@@ -1,35 +1,23 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ServerReadFromClientThread extends Thread {
     private ServerReadFromClientThread next;
-    private int batches;
+    private AtomicInteger batches;
     private Socket socket;
     private DataInputStream inputStream;
     private DataOutputStream outputStream;
 
     public ServerReadFromClientThread(Socket socket, int batchSize) {
         this.next = null;
-        this.batches = 0;
+        this.batches = new AtomicInteger(0);
         try {
             this.socket = socket;
             this.inputStream = new DataInputStream(socket.getInputStream());
             this.outputStream = new DataOutputStream(socket.getOutputStream());
             outputStream.writeInt(batchSize);
-            new Thread(() -> {
-                while (true) {
-                    if (batches != 0) {
-                        System.out.printf("Sending batches to client %d%n", batches);
-                        try {
-                            outputStream.writeInt(batches);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        batches = 0;
-                    }
-                }
-            }).start();
         } catch (Exception e) {
             this.socket = null;
             this.inputStream = null;
@@ -47,20 +35,34 @@ public class ServerReadFromClientThread extends Thread {
     }
 
     public void setBatches(int batches) {
-        this.batches = batches;
+        this.batches.set(batches);
     }
 
     public void addBatch() {
-        this.batches++;
+        this.batches.set(this.batches.get() + 1);
     }
 
     @Override
     public void run() {
+        new Thread(() -> {
+            while (true) {
+                final int batches = this.batches.get();
+                if (batches != 0) {
+                    System.out.printf("Sending batches to client %d%n", batches);
+                    try {
+                        outputStream.writeInt(batches);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    this.batches.set(0);
+                }
+            }
+        }).start();
+
         while (true) {
             try {
-                System.out.println("Player");
+                System.out.println("Player - blocking");
 
-                // TODO fix blocking operation
                 final int batchComplete = inputStream.readInt();
                 System.out.println("Player received message from client");
                 if (next == null) {
