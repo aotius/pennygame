19ef89e9aa5106cfac35b133e388c01a96f61464
devanevan <evanbourq@gmail.com>
2021-05-18 16:31:34 -0400

    Mac icon added

--- src/MainApplicationScreen.java
+++ src/MainApplicationScreen.java
@@ -1,3 +1,4 @@
+import com.sun.tools.javac.Main;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -9,10 +10,32 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.awt.*;
+import java.net.URL;
+
+
 public class MainApplicationScreen extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
+        //loading an image from a file
+        final Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
+        final URL imageResource = Main.class.getClassLoader().getResource("resources/icon.png");
+        final java.awt.Image image = defaultToolkit.getImage(imageResource);
+
+        //this is new since JDK 9
+        final Taskbar taskbar = Taskbar.getTaskbar();
+
+        try {
+            //set icon for mac os (and other systems which do support this method)
+            taskbar.setIconImage(image);
+        } catch (final UnsupportedOperationException e) {
+            System.out.println("The os does not support: 'taskbar.setIconImage'");
+        } catch (final SecurityException e) {
+            System.out.println("There was a security exception for: 'taskbar.setIconImage'");
+        }
+
         final VBox vbox = new VBox();
 
         vbox.setPadding(new Insets(10,10,10,10));
