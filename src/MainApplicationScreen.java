8779139246d84dd677fd805150bbf57755141118
aotius <66664792+aotius@users.noreply.github.com>
2021-05-18 16:25:37 -0400

    UI updates

--- src/MainApplicationScreen.java
+++ src/MainApplicationScreen.java
@@ -1,8 +1,10 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -12,11 +14,18 @@ public class MainApplicationScreen extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         final VBox vbox = new VBox();
-        final HBox hBoxTop = new HBox();
-        hBoxTop.setAlignment(Pos.CENTER);
-        final HBox hBoxBottom = new HBox();
 
+        vbox.setPadding(new Insets(10,10,10,10));
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(20);
+
+        final Image titlelogo = new Image(getClass().getResourceAsStream("resources/titlescreen.png"));
+        final ImageView title = new ImageView();
+        title.setImage(titlelogo);
+        vbox.getChildren().add(title);
         final Button button = new Button("Host Game");
+        button.setPrefSize(150,50);
+        button.setStyle("-fx.font-size:40");
         button.setOnAction(event -> {
             try {
                 //  TODO get info and set info on batch size
@@ -30,6 +39,8 @@ public class MainApplicationScreen extends Application {
         });
 
         final Button button2 = new Button("Join Game");
+        button2.setPrefSize(150,50);
+        button2.setStyle("-fx.font-size:40");
         button2.setOnAction(event -> {
             try {
                 PennyClient client = new PennyClient(stage);
@@ -38,15 +49,12 @@ public class MainApplicationScreen extends Application {
             }
         });
 
-        hBoxTop.getChildren().add(button);
-        hBoxTop.getChildren().add(button2);
-
-        vbox.getChildren().add(hBoxTop);
-        vbox.getChildren().add(hBoxBottom);
+        vbox.getChildren().add(button);
+        vbox.getChildren().add(button2);
 
         stage.setScene(new Scene(vbox));
-        stage.setWidth(800);
-        stage.setHeight(600);
+        stage.setWidth(750);
+        stage.setHeight(650);
         stage.setResizable(false);
         stage.setTitle("Agile Penny Game");
         stage.getIcons().add(new Image(getClass().getResourceAsStream("resources/icon.png")));
